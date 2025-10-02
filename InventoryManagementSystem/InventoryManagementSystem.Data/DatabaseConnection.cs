cfe76d9c1db9ca70937e4549f913153d1d8a3d30
Sofia Bernigola <smbernigola@gmail.com>
2025-10-02 16:09:24 -0300

    Add DatabaseConnection class and update project files
    Introduced DatabaseConnection.cs to encapsulate SQL Server database operations in the Data project. Updated .csproj files to include the new class and set Site.Master.cs SubType to ASPXCodeBehind. Modified Site.Master to specify language and set Bootstrap theme to dark.

---
+++ InventoryManagementSystem/InventoryManagementSystem.Data/DatabaseConnection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Data
+{
+    internal class DatabaseConnection
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+
+        public SqlDataReader Reader
+        {
+            get { return reader; }
+        }
+
+        public DatabaseConnection()
+        {
+            connection = new SqlConnection("server=.\\SQLEXPRESS; database=Inventory_Management; integrated security=true");
+            command = new SqlCommand();
+        }
+
+        public void setQuery(string consulta)
+        {
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = consulta;
+        }
+
+
+        public void execRead()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void execAction()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setParameter(string nombre, object valor)
+        {
+            command.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void closeConnection()
+        {
+            if (reader != null)
+                reader.Close();
+            connection.Close();
+        }
+    }
+}
